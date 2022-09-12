7cc258c033591223b8995716b43920ee32e3aba4
Murilo-Schlickmann <murilo.praesoft@gmail.com>
2022-09-12 08:19:20 -0300

    get-put

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import {Turma} from "./Turma"
-import { Docente } from "./Docente"
-import { Estudante } from "./Estudante"
+import { app } from "./app"
+import EstudanteEndpoint from "./endpoints/Estudante"
+import DocenteEndpoint from "./endpoints/Docente"
 
 
 const turma = new Turma(1, "Maria José", [], [], 0)
 console.log(turma)
+
+const estudante = new EstudanteEndpoint
+console.log(EstudanteEndpoint)
+
+const docente = new DocenteEndpoint
+console.log(DocenteEndpoint)
+
+app.post("/criar-turma", turma.criar)
+app.get("/buscar-turmas-ativas", turma.ativa)
+app.post("/mudar-modulo/:id", turma.mudarModulo)
+
+app.post("/criar-estudante", estudante.criar)
+app.get("/estudante/:nome", estudante.buscar)
+app.post("/mudar-estudante/:id", estudante.mudarTurma)
+
+app.post("/criar-docente", docente.criar)
+app.get("/docente/:nome",docente.buscar)
+app.post("/mudar-docene/:id", docente.mudarTurma)
